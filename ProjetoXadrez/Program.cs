c8b9b5968fc11dd4c546db86caa264dc1095ea35
Adriano Pinho <adriano.pynho@gmail.com>
2022-05-13 18:17:40 -0300

    Criado Classe Posicao

--- ProjetoXadrez/Program.cs
+++ ProjetoXadrez/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Tabuleiro;
+
+namespace ProjetoXadrez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao p;
+
+            p = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: " + p);
+        }
+    }
+}

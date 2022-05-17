0e3eaaa788ee1e8074751e4a629c8df5fb67a56d
Adriano Pinho <adriano.pynho@gmail.com>
2022-05-17 19:08:52 -0300

    Inicio Implementacao Movimentos Possiveis

--- ProjetoXadrez/Tabuleiro/Peca.cs
+++ ProjetoXadrez/Tabuleiro/Peca.cs
@@ -2,7 +2,7 @@
 
 namespace tabuleiro
 {
-    internal class Peca
+    internal abstract class Peca
     {
         public Posicao Posicao { get; set; }
         public Cor Cor { get; protected set; }
@@ -20,6 +20,11 @@ namespace tabuleiro
         public void incrementarQtdaMovimentos()
         {
             QtdaMovimentos++;
+        }
+
+        public abstract bool[,] movimentosPossiveis()
+        {
+
         }
     }
 }
--- ProjetoXadrez/xadrez/Torre.cs
+++ ProjetoXadrez/xadrez/Torre.cs
@@ -10,6 +10,23 @@ namespace xadrez
 
         }
 
+        private bool podeMover(Posicao pos)
+        {
+            Peca p = Tab.peca(pos);
+            return p == null || p.Cor != Cor;
+        }
+
+        public override bool[,] movimentosPossiveis()
+        {
+            bool[,] mat = new bool[Tab.Linhas, Tab.Colunas];
+            Posicao pos = new Posicao(0, 0);
+
+            //acima
+            pos.definirValores(Posicao.Linha - 1, Posicao.Coluna);
+            while()    
+            return mat;
+        }
+
         public override string ToString()
         {
             return "T";
